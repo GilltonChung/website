892bf7ff52f8dc25169d9720cef6e2e3cea59645
Gillton Chung <gilltonchh@gmail.com>
2022-05-22 17:13:49 +0000

    master

---
+++ connection/connect.php
@@ -0,0 +1,16 @@
+<?php
+
+//main connection file for both admin & front end
+$servername = "gilltonassignment2.mysql.database.azure.com"; //server
+$username = "gilltonadmin"; //username
+$password = "Jugular59"; //password
+$dbname = "online_rest";  //database
+
+// Create connection
+$db = mysqli_connect($servername, $username, $password, $dbname); // connecting 
+// Check connection
+if (!$db) {       //checking connection to DB	
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
